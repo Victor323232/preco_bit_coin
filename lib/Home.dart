c0f7077f1dab1c4ee224f3f6bcc4dc2cffe63ea0
Victor323232 <victorhugoabc59@gmail.com>
2022-05-23 11:53:01 -0300

    add Button

--- lib/Home.dart
+++ lib/Home.dart
@@ -21,7 +21,7 @@ class _HomeState extends State<Home> {
             Image.asset("images/logop.png"),
             ElevatedButton(
                 onPressed:(){},
-                child: Text(""))
+                child: Text("Atualizar"))
           ],
         ),
       ),
